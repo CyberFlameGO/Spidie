a8a1a7b1bf2cab479aa9e7bdf2817c77ed0dcd6e
Eros <george.marr.cobc@gmail.com>
2018-07-31 16:18:56 +0100

    adds in more functionality

--- spydie.py
+++ spydie.py
@@ -7,6 +7,46 @@ import re
 http = urllib3.PoolManager()
 
 
+def surf(data, url):
+    for link in data.find_all("a"):
+        link = link.get("href")
+
+        if link.startswith("/"):
+            link = url + link
+            soup = get_url_data(link)
+            values = get_page_data(soup)
+            json_update(values[0], values[1], values[2], link)
+        elif link.startswith("./"):
+            link = link.replace(".", url)
+            soup = get_url_data(link)
+            values = get_page_data(soup)
+            json_update(values[0], values[1], values[2], link)
+        else:
+            soup = get_url_data(link)
+            values = get_page_data(soup)
+            json_update(values[0], values[1], values[2], link)
+
+def get_page_data(data):
+    desc = ""
+    content = ""
+
+    for meta in data.find_all("meta", {"name": "description"}):
+        if meta:
+            desc = meta["content"]
+    for meta in data.find_all("meta", {"name": "keywords"}):
+        if meta:
+            content = meta["content"]
+    title = data.title.string
+    return [desc, content, title]
+
+
+def get_url_data(url):
+    response = http.request("GET", url)
+    html = response.data
+    soup = BeautifulSoup(html, "html.parser")
+    return soup
+
+
 def json_update(title, description, keywords, url):
     if title == "":
         title = description
