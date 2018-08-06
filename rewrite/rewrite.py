62b278ca07f5e8a3f1502f0bcaf506dc60a35ed7
Eros <george.marr.cobc@gmail.com>
2018-08-06 19:40:52 +0100

    awrkl[

--- rewrite/rewrite.py
+++ rewrite/rewrite.py
@@ -42,3 +42,37 @@ def create_directory(dir):
     if not os.path.exists(dir):
         print('[+] Creating new directory ' + dir)
         os.makedirs(dir)
+
+
+def create_files(proj_name, base_url):
+    queue = os.path.join(proj_name, 'queue.txt')
+    crawled = os.path.join(proj_name, 'crawled.txt')
+
+    if not os.path.isfile(queue):
+        write_file(queue, base_url)
+    if not os.path.isfile(crawled):
+        write_file(crawled, base_url)
+
+
+def write_file(path, data):
+    with open(path, 'w') as f:
+        f.write(data)
+
+
+def append_file(path, data):
+    with open(path, 'a') as file:
+        file.write(data)
+
+
+def append_to_file(path, data):
+    with open(path, 'a') as file:
+        file.write(data + '\n')
+
+
+def convert_file_to_set(file_name):
+    results = set()
+
+    with open(file_name, 'rt') as file:
+        for line in file:
+            results.add(line.replace('\n', ''))
+    return results
