96b8d9290b7c4728e4b3ca17f2bc91b778fe2e95
Eros <george.marr.cobc@gmail.com>
2018-08-06 19:36:18 +0100

    end me

--- rewrite/rewrite.py
+++ rewrite/rewrite.py
@@ -1,4 +1,8 @@
 from urllib.parse import urlparse
+from urllib import parse
+from html.parser import HTMLParser
+import os
+
 
 def get_domain_name(url):
     try:
@@ -6,8 +10,35 @@ def get_domain_name(url):
         return results[-2] + '.' + results[-1]
     except:
         return ''
+
+
 def get_sub_domain_name(url):
     try:
         return urlparse(url).netloc
     except:
         return ''
+
+
+class LinkFinder(HTMLParser):
+
+    def __init__(self, baseURL, pageURL):
+        super().__init__()
+        self.baseURL = baseURL
+        self.pageURL = pageURL
+        self.links = set()
+
+        def handle_star_tag(self, tag, attr):
+            if tag == 'a':
+                for attribute, value in attr:
+                    if attribute == 'href':
+                        url = parse.urljoin(self.baseURL, value)
+                        self.links.add(url)
+
+        def page_links(self):
+            return self.links
+
+
+def create_directory(dir):
+    if not os.path.exists(dir):
+        print('[+] Creating new directory ' + dir)
+        os.makedirs(dir)
